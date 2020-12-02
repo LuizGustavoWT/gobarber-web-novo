5639128705e2328250a00ce7c8fbe46a7e15a5f6
LuizGustavoWT <gustavoweberthums@hotmail.com>
2020-12-01 22:42:44 -0300

    GoBarber Web Nivel 3

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Route from './Route';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" component={SignIn} exact />
+      <Route path="/singup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+    </Switch>
+  );
+};
+
+export default Routes;
